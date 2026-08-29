3946077f6de2a739eb96ef66383b70ce1383dc54
kumarsauravcodes <saurav.ascentialabs@gmail.com>
2026-08-29 18:10:11 +0530

    image changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: ['unslow-freezingly-euclid.ngrok-free.dev'],
+    allowedHosts: [".ngrok-free.dev"],
   },
   build: {
     outDir: "dist",
